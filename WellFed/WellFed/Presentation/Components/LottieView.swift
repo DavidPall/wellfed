8bea902c6428c2b4b737d028fd1cbb31abba3b64
David Pall <david.pall@accenture.com>
2022-05-28 20:43:06 +0300

    -tips added -lottie added

---
+++ WellFed/WellFed/Presentation/Components/LottieView.swift
@@ -0,0 +1,37 @@
+//
+//  LottieView.swift
+//  WellFed
+//
+//  Created by Pall David on 28.05.2022.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .playOnce
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = AnimationView()
+        let animation = Animation.named(name)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
--- WellFed/WellFed/Presentation/DonateFood/DonatorProfile/DonatorProfileView.swift
+++ WellFed/WellFed/Presentation/DonateFood/DonatorProfile/DonatorProfileView.swift
@@ -13,18 +13,156 @@ struct DonatorProfileView: View {
     
     var body: some View {
         NavigationView {
-            Text("Profile")
-                .navigationTitle("Profile")
-                .navigationBarTitleDisplayMode(.large)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button {
-                            homeState = .Home
-                        } label: {
-                            Image(systemName: "house.fill")
-                        }
+            ScrollView {
+                content
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        homeState = .Home
+                    } label: {
+                        Image(systemName: "house.fill")
                     }
                 }
+            }
         }
+    }
+    
+    private var content: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Image(uiImage: UIImage(named: "profilePicture") ?? UIImage())
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    Text("John Doe").font(.largeTitle).bold()
+                }
+                Spacer()
+            }.padding()
+                .padding(.leading)
+            statistics
+            tips
+            Spacer()
+            buttons
+        }
+    }
+                    
+    private var tips: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ZStack {
+                    VStack {
+                        HStack {
+                            Text("Buy only what you need").bold()
+                                .padding()
+                            Spacer()
+                        }
+                        Text("Plan your meals. Make a shopping list and stick to it, and avoid impulse buys. Not only will you waste less food, you’ll also save money!")
+                            .padding([.horizontal, .bottom])
+                        Spacer()
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundColor(Color(UIColor.systemGray5))
+                            .shadow(radius: 5)
+                            .padding()
+                    }
+                    VStack {
+                        HStack {
+                            Spacer()
+                            LottieView(name: "lightBulb.json", loopMode: .loop)
+                                .frame(width: 70, height: 70)
+                                .offset(x: 10, y: -10)
+                        }
+                        Spacer()
+                    }
+                }
+                ZStack {
+                    VStack {
+                        HStack {
+                            Text("Sharing is caring").bold()
+                                .padding()
+                            Spacer()
+                        }
+                        Text("Donate food that would otherwise be wasted. For example, Apps can connect neighbours with each other and with local businesses so surplus food can be shared, not thrown away.")
+                            .padding([.horizontal, .bottom])
+                        Spacer()
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundColor(Color(UIColor.systemGray5))
+                            .shadow(radius: 5)
+                            .padding()
+                    }
+                    VStack {
+                        HStack {
+                            Spacer()
+                            LottieView(name: "lightBulb.json", loopMode: .loop)
+                                .frame(width: 70, height: 70)
+                                .offset(x: 10, y: -10)
+                        }
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+private var buttons: some View {
+    VStack {
+        NavigationLink("Terms and Conditions", destination: {})
+        Text("Log out")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 18)
+                    .foregroundColor(.gray.opacity(0.3))
+            }
+    }.padding()
+}
+
+private var statistics: some View {
+    HStack {
+        VStack(alignment: .leading) {
+            Text("Total donations")
+                .font(.caption)
+                .foregroundColor(.gray)
+            Text("53").font(.largeTitle)
+        }
+        Divider()
+            .frame(height: 80)
+            .padding()
+        VStack(alignment: .leading) {
+            Text("Donations this month")
+                .font(.caption)
+                .foregroundColor(.gray)
+            Text("12").font(.largeTitle)
+        }
+        Divider()
+            .frame(height: 80)
+            .padding()
+        VStack(alignment: .leading) {
+            Text("Rating \n")
+                .font(.caption)
+                .foregroundColor(.gray)
+            HStack {
+                Text("4.6").font(.largeTitle)
+                Image(systemName: "star.fill").foregroundColor(.yellow)
+            }
+        }
+    }.padding()
+}
+
+
+struct DonatorProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        DonatorProfileView(homeState: .constant(.Donate))
     }
 }
